3e27ba42486848d2d2f27e84b912e09ce60ef0be
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2022-12-29 01:03:48 -0500

    Added a2b function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
-
-print("Git works!")
-# openCV library analysis
-# import and implement pycharm instruction guide
-# gui lib/framework
+def a2b(char: str) -> str:
+    """
+    A function that returns the binary representation of an ASCII character
+    :param char:
+    :return the binary representation of an ASCII character (8 digits long):
+    """
+    # string.encode() function turns the specified string into an array of bytes
+    byte_array = char.encode()
+    # Convert the byte_array into a binary integer
+    binary_int = int.from_bytes(byte_array, "big")
+    # Convert binary_int to a string of binary characters
+    binary_string = bin(binary_int)
+    return binary_string[0] + binary_string[2:]
