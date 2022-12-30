a95c69f7d47a661fd7ea727379059471742b2fc1
HasNas03 <109939049+HasNas03@users.noreply.github.com>
2022-12-30 00:27:47 -0500

    Expanded Embedder.py

--- Embedder.py
+++ Embedder.py
@@ -1,5 +1,22 @@
 
-
-
 class Embedder():
+    """
+    The main class handling text-embedding into the image
 
+    === Attributes ===
+    text: The text to be embedded into the image
+    length: The length of the text
+    image: The path to the image
+    """
+
+    def __int__(self, text: str, image: str) -> None:
+        self.text = text
+        self.length = len(text)
+        self.image = image
+
+    def enough_pixels(self) -> int:
+        """
+
+        :return:
+        """
+        return self.length * 8
